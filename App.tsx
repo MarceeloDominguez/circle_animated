0f06fd871eb1bdeb15901c3ec19ae243d17af165
marcelo dominguez <marceelodominguez@gmail.com>
2023-04-25 16:14:07 -0300

    fix ui

--- App.tsx
+++ App.tsx
@@ -4,74 +4,15 @@ import { MaterialIcons } from "@expo/vector-icons";
 import {
   Animated,
   StyleSheet,
-  Text,
   TouchableOpacity,
   View,
   Dimensions,
   Image,
+  Text,
 } from "react-native";
+import { colors, data } from "./data";
 
-const data = [
-  {
-    image:
-      "https://i.pinimg.com/564x/45/17/a4/4517a4a7c41cce31bc78939a264539f2.jpg",
-  },
-  {
-    image:
-      "https://i.pinimg.com/564x/45/17/a4/4517a4a7c41cce31bc78939a264539f2.jpg",
-  },
-  {
-    image:
-      "https://i.pinimg.com/564x/45/17/a4/4517a4a7c41cce31bc78939a264539f2.jpg",
-  },
-  {
-    image:
-      "https://i.pinimg.com/564x/45/17/a4/4517a4a7c41cce31bc78939a264539f2.jpg",
-  },
-  {
-    image:
-      "https://i.pinimg.com/564x/45/17/a4/4517a4a7c41cce31bc78939a264539f2.jpg",
-  },
-  {
-    image:
-      "https://i.pinimg.com/564x/45/17/a4/4517a4a7c41cce31bc78939a264539f2.jpg",
-  },
-];
-
-const colors = [
-  {
-    initialBgColor: "goldenrod",
-    bgColor: "#222",
-    nextBgColor: "goldenrod",
-  },
-  {
-    initialBgColor: "#222",
-    bgColor: "goldenrod",
-    nextBgColor: "yellowgreen",
-  },
-  {
-    initialBgColor: "goldenrod",
-    bgColor: "yellowgreen",
-    nextBgColor: "midnightblue",
-  },
-  {
-    initialBgColor: "yellowgreen",
-    bgColor: "midnightblue",
-    nextBgColor: "turquoise",
-  },
-  {
-    initialBgColor: "midnightblue",
-    bgColor: "turquoise",
-    nextBgColor: "goldenrod",
-  },
-  {
-    initialBgColor: "turquoise",
-    bgColor: "goldenrod",
-    nextBgColor: "#222",
-  },
-];
-
-const { width } = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
 
 type Props = {
   onPress: () => void;
@@ -132,7 +73,7 @@ const Circle = ({ onPress, animatedValue, animatedValue2, index }: Props) => {
               {
                 scale: animatedValue2.interpolate({
                   inputRange: [0, 0.5, 1],
-                  outputRange: [1, 8, 1],
+                  outputRange: [1, 10, 1],
                 }),
               },
               {
@@ -176,7 +117,7 @@ const Circle = ({ onPress, animatedValue, animatedValue2, index }: Props) => {
               },
             ]}
           >
-            <MaterialIcons name="arrow-forward-ios" size={24} color="red" />
+            <MaterialIcons name="arrow-forward-ios" size={16} color="#fff" />
           </Animated.View>
         </TouchableOpacity>
       </Animated.View>
@@ -189,111 +130,77 @@ export default function App() {
   const animatedValue2 = useRef(new Animated.Value(0)).current;
   const sliderAnimatedValue = useRef(new Animated.Value(0)).current;
   const [index, setIndex] = useState(0);
-  console.log(index, "INDEX");
 
-  //gira para un lado y despuees vuelve cuando apreto el boton
-  // const animation = (toValue: number) =>
-  //   Animated.timing(animatedValue, {
-  //     toValue: toValue,
-  //     duration: 3000,
-  //     useNativeDriver: false,
-  //   });
-
-  //que gire siempre para el mismo lado cuando apreto el boton
   const animation = (toValue: number) =>
     Animated.parallel([
       Animated.timing(sliderAnimatedValue, {
         toValue: toValue,
-        duration: 3000,
+        duration: 2500,
         useNativeDriver: true,
       }),
       Animated.timing(animatedValue, {
         toValue: 1,
-        duration: 3000,
+        duration: 2500,
         useNativeDriver: true,
       }),
       Animated.timing(animatedValue2, {
         toValue: 1,
-        duration: 3000,
+        duration: 2500,
         useNativeDriver: false,
       }),
     ]);
 
   const onPress = () => {
-    //gira para un lado y despuees vuelve cuando apreto el boton
-    // setIndex(index === 1 ? 0 : 1);
-    // animation(index === 1 ? 0 : 1).start();
-    //*****/
-    //que gire siempre para el mismo lado cuando apreto el boton
     animatedValue.setValue(0);
     animatedValue2.setValue(0);
     animation((index + 1) % colors.length).start();
     setIndex((index + 1) % colors.length);
-
-    // animation(index + 1).start();
-    // if (index === colors.length - 1) {
-    //   return setIndex(0);
-    // }
-    // setIndex(index + 1);
   };
 
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
       <Circle
         onPress={onPress}
         animatedValue={animatedValue}
         animatedValue2={animatedValue2}
         index={index}
       />
-      <Animated.View
-        style={{
-          flexDirection: "row",
-          transform: [
-            {
-              translateX: sliderAnimatedValue.interpolate({
-                inputRange: [...Array(data.length).keys()],
-                outputRange:
-                  index === 0
-                    ? data.map((_, i) => i * width * 2)
-                    : data.map((_, i) => -i * width * 2),
-              }),
-            },
-          ],
-          opacity: sliderAnimatedValue.interpolate({
-            inputRange: [...Array(data.length * 2 + 1).keys()].map(
-              (i) => i / 2
-            ),
-            outputRange: [...Array(data.length * 2 + 1).keys()].map((i) =>
-              i % 2 === 0 ? 1 : 0
-            ),
-          }),
-        }}
-      >
-        {data.map(({ image }, index) => {
-          return (
-            <View
-              key={index}
-              style={{
-                width: width * 2,
-                paddingRight: width,
-                alignItems: "center",
-              }}
-            >
-              <Image
-                source={{ uri: image }}
-                style={{
-                  width: width * 0.65,
-                  height: 350,
-                  borderRadius: 16,
-                  //opacity: index === 0 ? 0 : 1,
-                }}
-                resizeMode="stretch"
-              />
-            </View>
-          );
-        })}
-      </Animated.View>
+      <View>
+        <Animated.View
+          style={{
+            flexDirection: "row",
+            transform: [
+              {
+                translateX: sliderAnimatedValue.interpolate({
+                  inputRange: [...Array(data.length).keys()],
+                  outputRange:
+                    index === 0
+                      ? data.map((_, i) => +i * width * 2)
+                      : data.map((_, i) => -i * width * 2),
+                }),
+              },
+            ],
+            opacity: sliderAnimatedValue.interpolate({
+              inputRange: [...Array(data.length * 2 + 1).keys()].map(
+                (i) => i / 2
+              ),
+              outputRange: [...Array(data.length * 2 + 1).keys()].map((i) =>
+                i % 2 === 0 ? 1 : 0
+              ),
+            }),
+          }}
+        >
+          {data.map(({ image }, index) => {
+            return (
+              <View key={index} style={styles.containerImage}>
+                <Text style={styles.title}>¡Hello World!</Text>
+                <Image source={{ uri: image }} style={styles.image} />
+              </View>
+            );
+          })}
+        </Animated.View>
+      </View>
     </View>
   );
 }
@@ -302,25 +209,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "flex-start",
-    paddingTop: 100,
+    paddingTop: height > 592 ? 90 : 50,
   },
   circleContainer: {
     backgroundColor: "gold",
-    flex: 1,
     alignItems: "center",
     justifyContent: "flex-end",
-    paddingBottom: 100,
-    padding: 8,
+    paddingBottom: height > 592 ? 90 : 30,
   },
   circle: {
     backgroundColor: "#444",
-    width: 100,
-    height: 100,
-    borderRadius: 100 / 2,
+    width: 60,
+    height: 60,
+    borderRadius: 60 / 2,
   },
   circleButton: {
     backgroundColor: "transparent",
     alignItems: "center",
     justifyContent: "center",
+  },
+  containerImage: {
+    width: width * 2,
+    paddingRight: width,
+    alignItems: "center",
+  },
+  image: {
+    width: width * 0.65,
+    height: height * 0.5,
+    borderRadius: 16,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 25,
+    letterSpacing: 0.5,
+    marginBottom: 22,
+    color: "#fff",
   },
 });
---
+++ data.ts
@@ -0,0 +1,59 @@
+export const data = [
+  {
+    image:
+      "https://i.pinimg.com/236x/09/c4/29/09c429ac7b934e66bfe580dc5f4612e7.jpg",
+  },
+  {
+    image:
+      "https://i.pinimg.com/236x/28/4a/9a/284a9a53e3382f92b95b29a393156b83.jpg",
+  },
+  {
+    image:
+      "https://i.pinimg.com/236x/ac/81/c3/ac81c3b168dbdb7f9e3f041289e0c17b.jpg",
+  },
+  {
+    image:
+      "https://i.pinimg.com/236x/9c/c2/dc/9cc2dc1d170654a2bf3b0991cc8f9d59.jpg",
+  },
+  {
+    image:
+      "https://i.pinimg.com/236x/ee/14/a5/ee14a5742b0fe9ba8765b5851dfe8b8e.jpg",
+  },
+  {
+    image:
+      "https://i.pinimg.com/236x/3b/1d/db/3b1ddbe6b0bcb8a659562c076ede51cc.jpg",
+  },
+];
+
+export const colors = [
+  {
+    initialBgColor: "#03001C",
+    bgColor: "#FA9884",
+    nextBgColor: "#FD841F",
+  },
+  {
+    initialBgColor: "#FA9884",
+    bgColor: "#FD841F",
+    nextBgColor: "#A75D5D",
+  },
+  {
+    initialBgColor: "#FD841F",
+    bgColor: "#A75D5D",
+    nextBgColor: "#205E61",
+  },
+  {
+    initialBgColor: "#A75D5D",
+    bgColor: "#205E61",
+    nextBgColor: "#16213E",
+  },
+  {
+    initialBgColor: "#205E61",
+    bgColor: "#16213E",
+    nextBgColor: "#03001C",
+  },
+  {
+    initialBgColor: "#16213E",
+    bgColor: "#03001C",
+    nextBgColor: "#FA9884",
+  },
+];
